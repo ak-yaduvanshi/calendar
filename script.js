36e7128477b63c8ad8f2734a8698024673a8d378
ak-yaduvanshi <155431309+ak-yaduvanshi@users.noreply.github.com>
2024-01-14 15:22:49 +0530

    third-project

---
+++ script.js
@@ -0,0 +1,71 @@
+let dt = new Date();
+
+
+function renderDate() {
+
+    document.querySelector("#date-str").innerHTML = dt.toDateString();
+
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "Octomber", "November", "December"];
+
+    let currentMonth = months[dt.getMonth()];
+    document.querySelector("#month").innerHTML = currentMonth;
+
+    // find the last date of current month
+    let endDate = new Date(dt.getFullYear(), dt.getMonth() + 1, 0);
+    endDate = endDate.getDate();
+    // console.log(endDate);
+
+    // find the day name of starting month date
+    dt.setDate(1);
+    let day = dt.getDay();
+
+    // find the previous months date 
+    let prevDate = new Date(dt.getFullYear(), dt.getMonth(), 0);
+    prevDate = prevDate.getDate();
+    // console.log(prevDate);
+
+    // find the today date
+    let today = new Date();
+    // console.log(today);
+
+    let cells = "";
+
+    for (x = day; x > 0; x--) {
+        cells += `<div class="prev-date">${prevDate - x + 1}</div>`;
+    }
+
+    for (i = 1; i <= endDate; i++) {
+        if (i == today.getDate() && dt.getMonth() == today.getMonth() && dt.getFullYear() == today.getFullYear()) {
+            cells += `<div class="today">${i}</div>`;
+            document.querySelector(".reset").style.display = "none";
+
+        }
+        else {
+            cells += `<div>${i}</div>`;
+        }
+    }
+
+    document.querySelector(".days").innerHTML = cells;
+
+}
+renderDate();
+document.querySelector(".reset").addEventListener("click", () => {
+    // dt = new Date(dt.getFullYear(),dt.getMonth(),0);
+    // console.log(dt);
+    location.reload();
+    // renderDate();
+})
+
+document.querySelector(".prev").addEventListener("click", () => {
+    dt.setMonth(dt.getMonth() - 1);
+    document.querySelector(".reset").style.display = "flex";
+    renderDate();
+})
+document.querySelector(".next").addEventListener("click", () => {
+    dt.setMonth(dt.getMonth() + 1);
+    document.querySelector(".reset").style.display = "flex";
+    renderDate();
+})
+
+
+
